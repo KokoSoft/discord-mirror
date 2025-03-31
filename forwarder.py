8d96a14c5daedfad7fa716521197ea87322c4f63
Koko <github@kokosoftware.pl>
2025-03-31 22:02:38 +0200

    forwarder: BotRunner: Add support for serialize sets
    Signed-off-by: Koko <github@kokosoftware.pl>

--- forwarder.py
+++ forwarder.py
@@ -789,6 +789,12 @@ class BotRunner():
 			level = log_level
 		)
 
+	@staticmethod
+	def serialize_sets(obj):
+		if isinstance(obj, set):
+			return list(obj)
+		return obj
+
 	def run(self):
 		try:
 			with open(self.session_file, 'r') as f:
@@ -815,4 +821,4 @@ class BotRunner():
 			loop.close()
 
 			with open(self.session_file, 'w', encoding='utf-8') as f:
-				json.dump(session, f, ensure_ascii=False, indent=4)
+				json.dump(session, f, ensure_ascii=False, indent=4, default=self.serialize_sets)
