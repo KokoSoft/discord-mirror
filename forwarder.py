4a7c02355e6efc9b81875be7a9d8cf4a4d9f2039
Koko <github@kokosoftware.pl>
2025-04-02 15:17:49 +0200

    forwarder: BotRunner: Make session file optional
    Signed-off-by: Koko <github@kokosoftware.pl>

--- forwarder.py
+++ forwarder.py
@@ -767,7 +767,7 @@ class Bot(discord_bot.Client, BotBase):
 class BotRunner():
 	def __init__(self,
 		bot,
-		sources : list[Config] = [],
+		sources : list[Client] = [],
 		session_file : str = "session.json",
 		log_level = discord_bot.utils.MISSING
 	):
@@ -788,11 +788,13 @@ class BotRunner():
 		return obj
 
 	def run(self):
-		try:
-			with open(self.session_file, 'r') as f:
-				session = json.load(f)
-		except FileNotFoundError:
-			session = {}
+		session = {}
+		if self.session_file:
+			try:
+				with open(self.session_file, 'r') as f:
+					session = json.load(f)
+			except FileNotFoundError:
+				pass
 
 		loop = asyncio.get_event_loop()
 		tasks = [loop.create_task(src.start(self.bot, session)) for src in self.sources]
@@ -812,5 +814,6 @@ class BotRunner():
 
 			loop.close()
 
-			with open(self.session_file, 'w', encoding='utf-8') as f:
-				json.dump(session, f, ensure_ascii=False, indent=4, default=self.serialize_sets)
+			if self.session_file:
+				with open(self.session_file, 'w', encoding='utf-8') as f:
+					json.dump(session, f, ensure_ascii=False, indent=4, default=self.serialize_sets)
